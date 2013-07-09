536f24fbc562aaf0cef3c18b10e16f9f32d672fd
Michael Elias <michaelelias88@gmail.com>
2013-07-09 19:36:57 +1000

    initial commit

---
+++ js/easeljs/version.js
@@ -0,0 +1,27 @@
+(function() {
+
+/**
+ * Static class holding library specific information such as the version and buildDate of
+ * the library.
+ * @class EaselJS
+ **/
+var o = this.createjs = this.createjs||{};
+o = (o.EaselJS = o.EaselJS||{});
+ 
+/**
+ * The version string for this release.
+ * @property version
+ * @type String
+ * @static
+ **/
+o.version = /*version*/"0.6.1"; // injected by build process
+
+/**
+ * The build date for this release in UTC format.
+ * @property buildDate
+ * @type String
+ * @static
+ **/
+o.buildDate = /*date*/"Tue, 14 May 2013 21:43:02 GMT"; // injected by build process
+
+})();
